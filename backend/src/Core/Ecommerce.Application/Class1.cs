96f950bb93b252c7b2473d8feb89c1120e6ec10f
DGARROSA77 <davidgarrosa77@gmail.com>
2023-08-16 16:15:25 +0200

    Service Generic

--- backend/src/Core/Ecommerce.Application/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Ecommerce.Application;
-
-public class Class1
-{
-
-}
---
+++ backend/src/Core/Ecommerce.Application/Persistence/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce.Application.Persistence;
+
+public interface IUnitOfWork : IDisposable{
+
+    IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : class;
+
+    Task<int> Complete();
+
+}
